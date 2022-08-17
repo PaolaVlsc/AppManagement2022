0ac70cdf9634374385c2f52b2c3a2b2b73a466b1
urmrs98 <paolavlsc170698@gmail.com>
2022-08-17 13:34:02 +0300

    Games header

--- Games.h
+++ Games.h
@@ -6,8 +6,32 @@
 #define APPMANAGEMENT2022_GAMES_H
 
 
-class Games {
+#include "Application.h"
 
+class Games : public Application {
+private:
+    string category;
+    bool isOnline;
+public:
+    // constructor
+    Games(const char *code, const string &title, Developer *developer, const string &category, bool isOnline);
+
+    // copy constructor
+    Games(const Games &);
+
+    // destructor
+    ~Games() override;
+
+    // setters
+    void setCategory(const string &category);
+    void setIsOnline(bool isOnline);
+
+    // getters
+    const string &getCategory() const;
+    bool getIsOnline() const;
+
+    // extra methods
+    // overloading operator
 };
 
 
