eee250b4c67dc26c01d1323152c61448ffad900e
urmrs98 <paolavlsc170698@gmail.com>
2022-08-17 13:21:13 +0300

    Developer header

--- Developer.cpp
+++ Developer.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "Developer.h"
+
+// constructor
+Developer::Developer(const char *code, const string &name, const string &email) : name(name),
+                                                                                  email(email) {
+    // management of const char *code attribute:
+
+
+}
+
+// copy constructor
+Developer::Developer(const Developer &) {
+
+}
+
+
+// destructor
+Developer::~Developer() {
+
+}
+
+// setters
+void Developer::setCode(const char *code) {
+    Developer::code = code;
+}
+
+void Developer::setName(const string &name) {
+    Developer::name = name;
+}
+
+void Developer::setEmail(const string &email) {
+    Developer::email = email;
+}
+
+// getters
+const char *Developer::getCode() const {
+    return code;
+}
+
+const string &Developer::getName() const {
+    return name;
+}
+
+const string &Developer::getEmail() const {
+    return email;
+}
+
+
+// extra methods
+
+
+// overloading
--- Developer.h
+++ Developer.h
@@ -6,9 +6,42 @@
 #define APPMANAGEMENT2022_DEVELOPER_H
 
 
-class Developer {
+#include <string>
 
+using std::string;
+
+class Developer {
+private:
+    const char *code;
+    string name;
+    string email;
+public:
+    // constructor
+    Developer(const char *, const string &, const string &);
+
+    // copy constructor
+    Developer(const Developer &);
+
+    // destructor
+    virtual ~Developer();
+
+    // setters
+    void setCode(const char *code);
+
+    void setName(const string &name);
+
+    void setEmail(const string &email);
+
+    // getters
+    const char *getCode() const;
+
+    const string &getName() const;
+
+    const string &getEmail() const;
+    // extra methods
 };
 
+
+// overloading operator <<
 
 #endif //APPMANAGEMENT2022_DEVELOPER_H
